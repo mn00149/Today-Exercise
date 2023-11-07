947dc688d872249220a24e7f5a4d3f1a81c276df
INWOONG OH <mn00149@naver.com>
2023-11-07 22:40:36 +0900

    first commit

---
+++ src/test/java/com/todayexercise/TodayexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todayexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
